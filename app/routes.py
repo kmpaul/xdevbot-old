3ab17ca288f4854969f25aab6612f4dcae1913c9
Kevin Paul <kpaul@ucar.edu>
2020-02-03 16:19:25 -0700

    First conditional template

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user)

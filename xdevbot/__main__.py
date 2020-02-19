f32cc4a2ec76007541dfb08d9555b2032d08deea
Kevin Paul <kpaul@ucar.edu>
2020-02-19 14:55:25 -0700

    Placeholder for default config

--- xdevbot/__main__.py
+++ xdevbot/__main__.py
@@ -2,4 +2,7 @@ import sys
 
 from .main import main
 
-main(sys.argv[1:], prog_name='xdevbot', auto_envvar_prefix='XDEV', default_map={})
+# Read default_map from config files with ConfigParser
+default_map = {}
+
+main(sys.argv[1:], prog_name='xdevbot', auto_envvar_prefix='XDEV', default_map=default_map)

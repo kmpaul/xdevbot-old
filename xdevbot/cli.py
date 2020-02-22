8bddbeb58c3c587a0e458351c8da060aa0d8d889
Kevin Paul <kpaul@ucar.edu>
2020-02-21 17:12:18 -0700

    Corrected CLI

--- xdevbot/cli.py
+++ xdevbot/cli.py
@@ -1,4 +1,5 @@
 import logging
+from configparser import ConfigParser
 
 import aiohttp_jinja2
 import click
@@ -7,14 +8,15 @@ from aiohttp import web
 
 from .middlewares import setup_middlewares
 from .routes import setup_routes
-from .settings import config_command
+
+DEFAULT_CONFIG_PATHS = ['config.ini']
 
 
 async def init_app(config=None):
 
     app = web.Application()
     app['config'] = config
-    aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('xdevbot', 'templates'))
+    aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader(__package__, 'templates'))
 
     # app.on_startup.append(load_db)
     # app.on_cleanup.append(save_db)
@@ -25,13 +27,38 @@ async def init_app(config=None):
     return app
 
 
-@click.command(cls=config_command(config_section='xdevbot'))
+def config_callback(ctx, config_param, config_file):
+    print('IN CALLBACK:')
+    for prm in ctx.command.params:
+        print(f'   {prm.name}: {prm.default}')
+    print(f'   * {config_param.name}: {config_file}')
+    config_paths = DEFAULT_CONFIG_PATHS + ([config_file] if config_file else [])
+    cfgparser = ConfigParser()
+    cfgparser.read(config_paths)
+    config = dict(cfgparser[__package__]) if __package__ in cfgparser else dict()
+    for param in ctx.command.params:
+        if param.name in config:
+            param.default = config[param.name]
+
+
+@click.command()
 @click.version_option(prog_name='Xdev Bot', version='0.0.1')
 @click.option('--host', default=None, type=str, help='Server IP address')
 @click.option('--port', default=None, type=int, help='Server port number')
 @click.option('--logging', default=logging.INFO, help='Logging output level')
+@click.option(
+    '--config',
+    default=None,
+    type=click.Path(exists=True),
+    help='User-defined configuration file location',
+    is_eager=True,
+    expose_value=False,
+    callback=config_callback,
+)
 def cli(**config):
-    print(config)
+    print('IN INVOCATION:')
+    for param in config:
+        print(f'   {param}: {config[param]}')
     logging.basicConfig(level=config['logging'])
     app = init_app(config=config)
     web.run_app(app, host=config['host'], port=config['port'])

1b2b1b53418bcf4806cc733cc3a3f486236b35c9
Kevin Paul <kpaul@ucar.edu>
2020-03-19 11:45:14 -0600

    Adding CLI tests

---
+++ xdevbot/tests/conftest.py
@@ -0,0 +1,22 @@
+import logging
+
+import pytest
+
+
+@pytest.fixture
+def cli_env(monkeypatch):
+    monkeypatch.setenv('XDEV_HOST', 'http://127.0.0.1')
+    monkeypatch.setenv('XDEV_PORT', '6789')
+    monkeypatch.setenv('LOGGING', logging.CRITICAL)
+
+
+@pytest.fixture
+def cli_configfile(tmpdir):
+    f = tmpdir.mkdir('config').join('config.ini')
+    f.write(
+        """[xdevbot]
+host = http://0.0.0.0
+port = 9999
+"""
+    )
+    return f
---
+++ xdevbot/tests/test_cli.py
@@ -0,0 +1,95 @@
+import logging
+
+import click
+
+from xdevbot.cli import cli
+
+
+def click_test_cmd(cmd: click.Command):
+    @click.command(cmd.name)
+    def test_cmd(**kwargs):
+        click.echo(kwargs)
+
+    test_cmd.params = cmd.params
+    return test_cmd
+
+
+def test_cli_defaults():
+    params = {'host': None, 'port': None, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='XDEV')
+    assert ctx.params == params
+
+
+def test_cli_host():
+    params = {'host': 'http://localhost', 'port': None, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(cli.name, args=['--host', params['host']], auto_envvar_prefix='XDEV')
+    assert ctx.params == params
+
+
+def test_cli_port():
+    params = {'host': None, 'port': 4567, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(
+        cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='XDEV'
+    )
+    assert ctx.params == params
+
+
+def test_cli_logging():
+    params = {'host': None, 'port': None, 'logging': logging.WARNING, 'mongodb': None}
+    ctx = cli.make_context(
+        cli.name, args=['--logging', str(params['logging'])], auto_envvar_prefix='XDEV'
+    )
+    assert ctx.params == params
+
+
+def test_cli_mongodb():
+    params = {
+        'host': None,
+        'port': None,
+        'logging': logging.INFO,
+        'mongodb': 'mongodb://test.mongodb.com',
+    }
+    ctx = cli.make_context(
+        cli.name, args=['--mongodb', params['mongodb']], auto_envvar_prefix='XDEV'
+    )
+    assert ctx.params == params
+
+
+def test_cli_env(cli_env):
+    params = {'host': 'http://127.0.0.1', 'port': 6789, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='XDEV')
+    assert ctx.params == params
+
+
+def test_cli_env_overridden(cli_env):
+    params = {'host': 'http://127.0.0.1', 'port': 9999, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(
+        cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='XDEV'
+    )
+    assert ctx.params == params
+
+
+def test_cli_configfile(cli_configfile):
+    params = {'host': 'http://0.0.0.0', 'port': 9999, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(cli.name, args=['--config', cli_configfile], auto_envvar_prefix='XDEV')
+    assert ctx.params == params
+
+
+def test_cli_configfile_overridden(cli_configfile):
+    params = {'host': 'http://0.0.0.0', 'port': 8888, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(
+        cli.name,
+        args=['--config', cli_configfile, '--port', str(params['port'])],
+        auto_envvar_prefix='XDEV',
+    )
+    assert ctx.params == params
+
+
+def test_cli_env_configfile_overridden(cli_env, cli_configfile):
+    params = {'host': 'http://127.0.0.1', 'port': 8888, 'logging': logging.INFO, 'mongodb': None}
+    ctx = cli.make_context(
+        cli.name,
+        args=['--config', cli_configfile, '--port', str(params['port'])],
+        auto_envvar_prefix='XDEV',
+    )
+    assert ctx.params == params

c212a3953f2a37dc4212a8146234c77781b1c0c8
Kevin Paul <kpaul@ucar.edu>
2020-03-25 11:08:48 -0600

    Testing default templating

--- xdevbot/tests/test_views.py
+++ xdevbot/tests/test_views.py
@@ -10,4 +10,4 @@ async def test_index(aiohttp_client, loop):
     assert resp.status == 200
     html_doc = await resp.text()
     soup = BeautifulSoup(html_doc, 'html.parser')
-    assert soup.title.string == 'Home'
+    assert soup.title.string == '[Title]'
--- xdevbot/views.py
+++ xdevbot/views.py
@@ -3,4 +3,4 @@ import aiohttp_jinja2
 
 @aiohttp_jinja2.template('index.html')
 async def index(request):
-    return {'title': 'Home', 'content': 'This is the home page.'}
+    return {}
